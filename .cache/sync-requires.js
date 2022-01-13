c192aae1cbacbb99bb2c172bce7394e647b43857
Yugal <ydihora@my.yorku.ca>
2022-01-12 22:40:55 -0500

    folder

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\pages\\page-2.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\pages\\using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\pages\\using-typescript.tsx")),
+  "component---src-templates-using-dsg-js": preferDefault(require("C:\\Users\\ydiho\\gatsby-starter-default\\src\\templates\\using-dsg.js"))
+}
+
